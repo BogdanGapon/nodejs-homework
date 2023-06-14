91f9237ce737f09d24e96928ce9af8e9edec369a
Bogdan <bogdangaponenko1996@gmail.com>
2023-06-14 14:04:52 -0500

    add branch to initialize it with render.com

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require('./app')
+const app = require('./app');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const { DB_HOST, HOST = 3000 } = process.env;
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose.connect(DB_HOST).then(() => {
+  //  подключение к базе данных
+
+  try {
+    app.listen(HOST);
+    // подключаем вер-сервер, после того как подключились к базе данных,
+    // так как нет смысла в подключении сервера, если у нас ошибка в получение данных с базы данных
+  } catch (err) {
+    console.log(err.message);
+    process.exit(1);
+  }
+});
