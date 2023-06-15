c8d2ece280d062b838f0dd4162cf25a94f2131df
Bogdan <bogdangaponenko1996@gmail.com>
2023-06-15 11:42:39 -0500

    solve the problem with deploy (forgot add environment variable to render.commit)

--- server.js
+++ server.js
@@ -3,19 +3,15 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const { DB_HOST, PORT } = process.env;
-console.log(typeof DB_HOST);
-mongoose.set('strictQuery', true);
-mongoose
-  .connect(DB_HOST, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    //  подключение к базе данных
+mongoose.connect(DB_HOST).then(() => {
+  //  подключение к базе данных
 
-    try {
-      app.listen(PORT);
-      // подключаем вер-сервер, после того как подключились к базе данных,
-      // так как нет смысла в подключении сервера, если у нас ошибка в получение данных с базы данных
-    } catch (err) {
-      console.log(err.message);
-      process.exit(1);
-    }
-  });
+  try {
+    app.listen(PORT);
+    // подключаем вер-сервер, после того как подключились к базе данных,
+    // так как нет смысла в подключении сервера, если у нас ошибка в получение данных с базы данных
+  } catch (err) {
+    console.log(err.message);
+    process.exit(1);
+  }
+});
